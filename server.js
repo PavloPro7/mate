36c0cc38a996fc072345fc449fd2081c8407fa77
PavloPro7 <03ppcv@gmail.com>
2026-03-20 12:59:37 +0100

    linked turned on a server

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express(); // Create an instance of the Express app
+const PORT = 3000; // You can change the port number if needed
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
