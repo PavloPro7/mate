cc8319d7351c48166bdca88a0061d034a6af04b7
PavloPro7 <03ppcv@gmail.com>
2026-04-20 23:03:34 +0200

    added instansec, how write correct loops (sorry for a while i don't did it)

--- hw.js
+++ hw.js
@@ -19,4 +19,25 @@ decimal: ${num.toString(10)},
 hexadecimal: ${num.toString(16)}`
 }
 
-console.log(toAllTypes(63));
+console.log(toAllTypes(20));
+
+const inventory = ['sword', 'shield', 'potion'];
+
+for (const item of inventory) {
+  console.log("You have a " + item);
+}
+
+// 1. Start: let i = 0
+// 2. Condition: run as long as i < 5
+// 3. Step: increase i by 1 after each loop (i++)
+for (let i = 0; i < 5; i++) {
+  console.log(i); // Prints 0, 1, 2, 3, 4
+}
+
+let count = 0;
+
+while (count < 3) {
+  console.log("Count is: " + count);
+  count++; // CRITICAL: Always change the condition inside, or it runs forever!
+}
+
