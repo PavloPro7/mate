3ba80cbdcd8318f610112be55291fbad21dc9b69
PavloPro7 <03ppcv@gmail.com>
2026-04-20 23:40:49 +0200

    did statistic rate

--- hw.js
+++ hw.js
@@ -12,32 +12,44 @@
 // } 
 
 // console.log(isWerewolf('level'));
-function toAllTypes(num) {
-    return `binary: ${num.toString(2)}, 
-octal: ${num.toString(8)},
-decimal: ${num.toString(10)}, 
-hexadecimal: ${num.toString(16)}`
+// function toAllTypes(num) {
+//     return `binary: ${num.toString(2)}, 
+// octal: ${num.toString(8)},
+// decimal: ${num.toString(10)}, 
+// hexadecimal: ${num.toString(16)}`
+// }
+
+// console.log(toAllTypes(20));
+
+// const inventory = ['sword', 'shield', 'potion'];
+
+// for (const item of inventory) {
+//   console.log("You have a " + item);
+// }
+
+// // 1. Start: let i = 0
+// // 2. Condition: run as long as i < 5
+// // 3. Step: increase i by 1 after each loop (i++)
+// for (let i = 0; i < 5; i++) {
+//   console.log(i); // Prints 0, 1, 2, 3, 4
+// }
+
+// let count = 0;
+
+// while (count < 3) {
+//   console.log("Count is: " + count);
+//   count++; // CRITICAL: Always change the condition inside, or it runs forever!
+// }
+
+function getSuccessRate(statistic) {
+  // write code here
+  let sum = statistic.length;
+  let succeed = 0;
+  for (let stat of statistic) {
+    if (stat == 1) {
+      succeed++;
+    } 
+  }
+  return (sum !== 0) ? 100 * succeed / sum : 0;
 }
-
-console.log(toAllTypes(20));
-
-const inventory = ['sword', 'shield', 'potion'];
-
-for (const item of inventory) {
-  console.log("You have a " + item);
-}
-
-// 1. Start: let i = 0
-// 2. Condition: run as long as i < 5
-// 3. Step: increase i by 1 after each loop (i++)
-for (let i = 0; i < 5; i++) {
-  console.log(i); // Prints 0, 1, 2, 3, 4
-}
-
-let count = 0;
-
-while (count < 3) {
-  console.log("Count is: " + count);
-  count++; // CRITICAL: Always change the condition inside, or it runs forever!
-}
-
+console.log(getSuccessRate('110111000001'));
