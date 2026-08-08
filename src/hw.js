3a46647a5c0c252f27babe4d044ca4cdb639a0c8
PavloPro7 <03ppcv@gmail.com>
2026-08-08 16:10:06 +0200

    add Object.assign instance

--- src/hw.js
+++ src/hw.js
@@ -1220,3 +1220,25 @@ const bobCopy = clone(bob);
 
 console.log(bobCopy === bob);
 console.log(bobCopy);
+
+const kobi = {
+  chipVer: 9,
+  serialNo: 413,
+  wheels: 2,
+};
+
+const parts = [
+  { wheels: 6 },
+  { chipVer: 16 },
+  { displays: 2 },
+];
+
+function upgradeRobot(robot, parts) {
+  for (const part of parts) {
+    Object.assign(robot, part);
+  }
+}
+
+upgradeRobot(kobi, parts);
+
+console.log(kobi);
