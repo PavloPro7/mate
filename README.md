fcfc83e87fc2d020a471f44e18fa5c44ad8c492c
PavloPro7 <03ppcv@gmail.com>
2026-08-10 15:35:10 +0200

    remove unused files: README.md, help.html, hw.js, index.html, products.html, server.js, and style.css

--- README.md
+++
@@ -1,3 +0,0 @@
-# mate
-
-This project is part of my learning path as Fullstack AI Developer
--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express(); // Create an instance of the Express app
-const PORT = 3000; // You can change the port number if needed
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
-'use strict';
-
